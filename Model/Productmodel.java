59ae76a356360a7ca22c071ed5cd4013ecbfaa45
Pallavi <acharpallavi565@gmail.com>
2026-03-27 15:18:50 +0530

    initial commit

---
+++ Model/Productmodel.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Product extends BaseModel {
+
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
